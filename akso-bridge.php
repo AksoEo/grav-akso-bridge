2827d69c3451c278c97335d2723f6be4c055215f
cpsdqs <cpsdqs@gmail.com>
2021-08-24 17:10:02 +0200

    Show support button when logged in

--- akso-bridge.php
+++ akso-bridge.php
@@ -306,9 +306,6 @@ class AksoBridgePlugin extends Plugin {
         $state['akso_auth'] = $this->aksoUser !== null;
         $state['akso_full_auth'] = $this->aksoUser ? !$this->aksoUser['totp'] : false;
         $state['akso_user_is_member'] = $this->aksoUser ? $this->aksoUser['member'] : false;
-        if (!isset($state['uea_hide_support_button'])) {
-            $state['uea_hide_support_button'] = $state['akso_user_is_member'];
-        }
         if ($this->aksoUser !== null) {
             $state['akso_user_fmt_name'] = $this->aksoUserFormattedName;
             $state['akso_uea_code'] = $this->aksoUser['uea'];
